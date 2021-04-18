c5a401e5afb5d4cea24afca81020181e4ed89fe2
BeerFairy <moodholly78@gmail.com>
2021-04-18 12:28:36 +0600

    added actor page

--- movies/views.py
+++ movies/views.py
@@ -2,21 +2,24 @@ from django.shortcuts import redirect
 from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 
-from movies.models import Movie, Category
+from movies.models import Movie, Category, Actor
 from movies.forms import ReviewFrom
 
 
 class MovieView(ListView):
+    '''Вывод всех фильмов'''
     model = Movie
     queryset = Movie.objects.filter(draft=False)
 
 
 class MovieDetail(DetailView):
+    '''Вывод информации о фильме'''
     model = Movie
     slug_field = 'url'
 
 
 class AddReview(View):
+    '''Отзывы'''
     def post(self, request, pk):
         form = ReviewFrom(request.POST)
         movie = Movie.objects.get(id=pk)
@@ -27,3 +30,10 @@ class AddReview(View):
             form.movie = movie
             form.save()
         return redirect(movie.get_absolute_url())
+
+
+class ActorView(DetailView):
+    '''Вывод информации о актере'''
+    model = Actor
+    template_name = 'movies/actor.html'
+    slug_field = 'name'
