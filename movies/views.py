ed3f089177f8b5cb18bc46e2e5ba53b2c2eb5a4b
BeerFairy <moodholly78@gmail.com>
2021-04-18 21:46:11 +0600

    added genre, year filters

--- movies/views.py
+++ movies/views.py
@@ -2,17 +2,27 @@ from django.shortcuts import redirect
 from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 
-from movies.models import Movie, Category, Actor
+from movies.models import Movie, Category, Actor, Genre
+from django.db.models import Q
 from movies.forms import ReviewFrom
 
 
-class MovieView(ListView):
+class GenreYear:
+
+    def get_genres(self):
+        return Genre.objects.all()
+
+    def get_years(self):
+        return Movie.objects.filter(draft=False).values('year')
+
+
+class MovieView(GenreYear, ListView):
     '''Вывод всех фильмов'''
     model = Movie
     queryset = Movie.objects.filter(draft=False)
 
 
-class MovieDetail(DetailView):
+class MovieDetail(GenreYear, DetailView):
     '''Вывод информации о фильме'''
     model = Movie
     slug_field = 'url'
@@ -32,8 +42,18 @@ class AddReview(View):
         return redirect(movie.get_absolute_url())
 
 
-class ActorView(DetailView):
+class ActorView(GenreYear, DetailView):
     '''Вывод информации о актере'''
     model = Actor
     template_name = 'movies/actor.html'
     slug_field = 'name'
+
+
+class FilterMoviesView(GenreYear ,ListView):
+    '''Фильтр фильмов'''
+    def get_queryset(self):
+        queryset = Movie.objects.filter(
+            Q(year__in=self.request.GET.getlist('year'))|
+            Q(genres__in=self.request.GET.getlist('genre'))
+            )
+        return queryset
