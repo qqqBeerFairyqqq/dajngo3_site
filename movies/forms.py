161736c13d9bbbdf9c807e25c4de9dce44208d9c
BeerFairy <moodholly78@gmail.com>
2021-04-19 11:00:07 +0600

    added rating stars

--- movies/forms.py
+++ movies/forms.py
@@ -1,8 +1,20 @@
 from django import forms
-from movies.models import Reviews
+from movies.models import Reviews, Rating, RatingStar
 
 
 class ReviewFrom(forms.ModelForm):
-    class Meta:
-        model = Reviews
-        fields = ('name', 'email', 'text', 'parent')
+	'''Форма отзывов'''
+	class Meta:
+		model = Reviews
+		fields = ('name', 'email', 'text', 'parent')
+
+
+class RatingForm(forms.ModelForm):
+	'''Форма добавления рейтинга'''
+	tar = forms.ModelChoiceField(
+		queryset=RatingStar.objects.all(), widget=forms.RadioSelect(), empty_label=None
+	)
+
+	class Meta:
+		model = Rating
+		fields = ("star",)
