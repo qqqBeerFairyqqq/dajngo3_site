9a23b9b3f41a9a83f37cbeb61cecd88e79b97e47
BeerFairy <moodholly78@gmail.com>
2021-04-23 14:58:02 +0600

    added authenication by vk messager

--- django_movie/settings.py
+++ django_movie/settings.py
@@ -48,6 +48,8 @@ INSTALLED_APPS = [
     'snowpenguin.django.recaptcha3',
     'allauth',
     'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.vk',
 
     'movies.apps.MoviesConfig',
     'maillings.apps.MaillingsConfig',
